c3829ce8e8989a607973826688c94135fbaeb9b1
alekspanteli <alekspanteli@rambler.ru>
2022-09-20 13:08:56 +0000

    Update Hero.js and globals.css

--- my-app/components/Hero.js
+++ my-app/components/Hero.js
@@ -1,8 +1,6 @@
-import React from "react";
-
 const Hero = () => {
   return (
-    <div className="flex items-center justify-center h-full">
+    <div className="flex items-center justify-center h-screen bg-fixed bg-center bg-cover hero-img">
       <div />
       <div>
         <h2>Heading</h2>
