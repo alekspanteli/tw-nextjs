6e164dbb321b56a214fb0cf8ef4f5eac9174a12a
alekspanteli <alekspanteli@rambler.ru>
2022-09-20 13:57:13 +0000

    Update 3 files

--- my-app/components/Hero.js
+++ my-app/components/Hero.js
@@ -1,13 +1,13 @@
-const Hero = () => {
+const Hero = ({ message, heading }) => {
   return (
     <div
       className="relative grid place-content-center h-screen 
     bg-fixed bg-center bg-cover hero-img isolate before:absolute before:inset-0 before:bg-black/50 before:z-[-1]"
     >
-      <div className="text-white">
-        <h2>Heading</h2>
-        <p>Message</p>
-        <button>Book</button>
+      <div className="text-white p-5">
+        <h2 className="text-5xl font-bold">{heading}</h2>
+        <p className="py-5 text-xl">{message}</p>
+        <button className="px-8 border">Book</button>
       </div>
     </div>
   );
---
+++ my-app/components/Navbar.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Captur</h1>
+      <ul>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
