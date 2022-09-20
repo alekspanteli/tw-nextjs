855f6e39712da880b3e0a3ba88d0fb6ccf710098
alekspanteli <alekspanteli@rambler.ru>
2022-09-20 13:04:49 +0000

    Update 3 files

---
+++ my-app/components/Hero.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex items-center justify-center h-full">
+      <div />
+      <div>
+        <h2>Heading</h2>
+        <p>Message</p>
+        <button>Book</button>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
