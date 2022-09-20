e0cb38677b959b0e17824911a9748f84be3ac6ff
alekspanteli <alekspanteli@rambler.ru>
2022-09-20 14:20:06 +0000

    Update 4 files

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -1,33 +1,47 @@
-import Link from 'next/link'
+import Link from 'next/link';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>
-      <Link href="/">
-        <h1>Captur</h1>
-      </Link>
-      <ul>
+    <div className="fixed left-0 top-0 z-[1] w-full duration-300">
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4">
+        <Link href="/">
+          <h1>Captur</h1>
+        </Link>
+        <ul>
           <li>
-              <Link href="/home">
-                  Home
-            </Link>
+            <Link href="/home">Home</Link>
           </li>
           <li>
-              <Link href="/#gallery">
-                  Gallery
-            </Link>
+            <Link href="/#gallery">Gallery</Link>
           </li>
           <li>
-              <Link href="/work">
-                  Work
-            </Link>
+            <Link href="/work">Work</Link>
           </li>
           <li>
-              <Link href="/contact">
-                  Contact
-            </Link>
+            <Link href="/contact">Contact</Link>
           </li>
-      </ul>
+        </ul>
+        <div>
+          <AiOutlineMenu size={20} />
+        </div>
+        <div>
+          <ul>
+            <li>
+              <Link href="/home">Home</Link>
+            </li>
+            <li>
+              <Link href="/#gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link href="/work">Work</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
