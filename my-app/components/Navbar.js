a0a421f1c804b811d41be052bfc1ee28e839d4f2
alekspanteli <alekspanteli@rambler.ru>
2022-09-20 14:04:09 +0000

    Update Navbar.js and _app.js

--- my-app/components/Navbar.js
+++ my-app/components/Navbar.js
@@ -3,8 +3,30 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <div>
-      <h1>Captur</h1>
+      <Link href="/">
+        <h1>Captur</h1>
+      </Link>
       <ul>
+          <li>
+              <Link href="/home">
+                  Home
+            </Link>
+          </li>
+          <li>
+              <Link href="/#gallery">
+                  Gallery
+            </Link>
+          </li>
+          <li>
+              <Link href="/work">
+                  Work
+            </Link>
+          </li>
+          <li>
+              <Link href="/contact">
+                  Contact
+            </Link>
+          </li>
       </ul>
     </div>
   );
