45054e1a2bafbab131dc734d639c845d138061de
alekspanteli <alekspanteli@rambler.ru>
2022-09-20 13:34:55 +0000

    Update Hero.js

--- my-app/components/Hero.js
+++ my-app/components/Hero.js
@@ -1,8 +1,10 @@
 const Hero = () => {
   return (
-    <div className="grid place-content-center h-screen bg-fixed bg-center bg-cover hero-img">
-      <div />
-      <div>
+    <div
+      className="relative grid place-content-center h-screen 
+    bg-fixed bg-center bg-cover hero-img isolate before:absolute before:inset-0 before:bg-black/50 before:z-[-1]"
+    >
+      <div className="text-white">
         <h2>Heading</h2>
         <p>Message</p>
         <button>Book</button>
